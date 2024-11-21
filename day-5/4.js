6b9ceaf738ff3386bc77134a4bc3d076f38befaa
HetVaishnani <hetvaishnani0709@gamil.com>
2024-11-21 18:31:34 +0530

    code

---
+++ day-5/4.js
@@ -0,0 +1,17 @@
+let arr = [1, 2, 3, 2, 4];
+
+function call(data, target) {
+    let newArr = [];
+
+    data.forEach((el, i) => {
+        if (target == el) {
+            newArr.push(i);
+        }
+    })
+
+    return newArr[newArr.length - 1];
+
+}
+
+console.log(call(arr, 2));
+console.log(call(arr, 5));
