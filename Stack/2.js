0bef7a0b9a5352bf3fdf5d05ee9483631fa7f6b7
HetVaishnani <hetvaishnani0709@gamil.com>
2024-12-09 18:38:19 +0530

    code

---
+++ Stack/2.js
@@ -0,0 +1,21 @@
+let str = "(())"
+let stack = []
+let obj = { ")": "(", "]": "[", "}": "{" }
+function isBalance() {
+    for (let x = 0; x < str.length; x++) {
+        if (str[x] == "(" || str[x] == "{" || str[x] == "[") {
+            stack.push(str[x])
+        } else if (str[x] == ")" || str[x] == "}" || str[x] == "]") {
+            if (stack.pop() != obj[str[x]]) {
+                return false
+            }
+        }
+    }
+    if (stack.length == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(isBalance());
