45abe2c6c8ecb648484c3fed614943d073fbdb28
HetVaishnani <hetvaishnani0709@gamil.com>
2024-12-09 23:07:10 +0530

    code

---
+++ Stack/4.js
@@ -0,0 +1,23 @@
+let char = "radar";
+
+function isPalindrome(str) {
+
+    let stack = [];
+
+    for (let char of str) {
+        stack.push(char)
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] != stack.pop()) {
+            return false
+        }
+    }
+
+    return true;
+
+}
+
+console.log(isPalindrome(char));
+console.log(isPalindrome("india"));
+console.log(isPalindrome("mrowlatemymetalworm")); 
